32be865a355d0121c6a163f0b19274dd040c5948
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2009-02-04 02:31:16 -0500

    story: time the rendering

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC=gcc
-CFLAGS=-Wall -g -O2 -std=c99
+CFLAGS=-Wall -g -O2 -std=c99 -D_POSIX_C_SOURCE=199309 -lrt
 
 FILES=sar.c post.c xattr.c html.c
 
--- story.c
+++ story.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include "post.h"
 #include "sar.h"
@@ -8,9 +9,12 @@
 
 int main(int argc, char **argv)
 {
+	struct timespec s,e;
 	char *path_info;
 	struct post post;
 	int ret;
+
+	clock_gettime(CLOCK_REALTIME, &s);
 
 	post.out = stdout;
 
@@ -37,6 +41,11 @@ int main(int argc, char **argv)
 	html_footer(&post);
 
 	destroy_post(&post);
+
+	clock_gettime(CLOCK_REALTIME, &e);
+
+	fprintf(post.out, "<!-- time to render: %ld.%09ld seconds -->\n", (int)e.tv_sec-s.tv_sec,
+		e.tv_nsec-s.tv_nsec+((e.tv_sec-s.tv_sec) ? 1000000000 : 0));
 
 	return 0;
 }
