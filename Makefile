1f795be4be9334549b006e518c02a184abc17ae1
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2011-02-16 18:09:18 -0500

    format 3 post: basic listing command
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CC=gcc
 CFLAGS=-Wall -g -O2 -std=c99 -D_POSIX_C_SOURCE=199309 -D_BSD_SOURCE -lrt
 
 FILES=sar.c post.c xattr.c html.c dir.c fsm.c decode.c post_fmt3.tab.c \
-	post_fmt3.lex.c
+	post_fmt3.lex.c listing.c
 BINS=story index archive category feed comment
 
 all: $(BINS)
---
+++ listing.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "listing.h"
+
+#define PAGE 4096
+
+#define COPYSTR(ptr, len, size, str)	do { \
+						int x = strlen(str); \
+						if ((len+x+1) > size) { \
+							size += PAGE; \
+							ptr = realloc(ptr, size); \
+						} \
+						strcpy(ptr+len, str); \
+						len += x; \
+					} while(0)
+
+#define COPYCHR(ptr, len, size, chr)	do { \
+						if ((len+2) > size) { \
+							size += PAGE; \
+							ptr = realloc(ptr, size); \
+						} \
+						ptr[len] = chr; \
+						len++; \
+					} while(0)
+
+char *listing(struct post *post, char *fname)
+{
+	char path[FILENAME_MAX];
+	char *out;
+	int len, size;
+	FILE *in;
+
+	snprintf(path, FILENAME_MAX, "data/posts/%d/%s", post->id, fname);
+
+	in = fopen(path, "r");
+	if (!in) {
+		snprintf(path, FILENAME_MAX, "Failed to open listing file '%s'.", fname);
+		return strdup(path);
+	}
+
+	len = 0;
+	size = PAGE;
+	out = malloc(PAGE);
+	if (!out)
+		goto err_mem;
+
+	while(1) {
+		char c;
+
+		c = fgetc(in);
+		if (feof(in))
+			break;
+
+		switch(c) {
+			case '&':
+				COPYSTR(out, len, size, "&amp;");
+				break;
+			case '<':
+				COPYSTR(out, len, size, "&lt;");
+				break;
+			case '>':
+				COPYSTR(out, len, size, "&gt;");
+				break;
+			default:
+				COPYCHR(out, len, size, c);
+				break;
+		}
+	}
+
+	fclose(in);
+	return out;
+
+err_mem:
+	snprintf(path, FILENAME_MAX, "Falied to allocate memory for buffer "
+		 "(len %d, size %d bytes).", len, size);
+	return strdup(path);
+}
