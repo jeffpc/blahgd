5516c8bafb93e14170c1a613ef881f208e657d07
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2013-01-07 20:31:11 -0500

    url escape tag names

--- pipeline.c
+++ pipeline.c
@@ -1,11 +1,86 @@
 #include <assert.h>
 #include <string.h>
 #include <stdio.h>
+#include <ctype.h>
 
 #include "pipeline.h"
 
 static struct var_val *nop_fxn(struct var_val *v)
 {
+	return v;
+}
+
+static struct var_val *urlescape_fxn(struct var_val *v)
+{
+	const char hd[16] = "0123456789ABCDEF";
+
+	int outlen;
+	char *out, *tmp;
+	char *s;
+
+	assert(v->type == VT_STR);
+	assert(v->str);
+
+	outlen = strlen(v->str);
+
+	for (s = v->str; *s; s++) {
+		char c = *s;
+
+		if (isalnum(c))
+			continue;
+
+		switch (c) {
+			case ' ':
+			case '-':
+			case '_':
+			case '.':
+			case '~':
+				continue;
+		}
+
+		/* this char will be escaped */
+		outlen += 2;
+	}
+
+	out = malloc(outlen + 1);
+	assert(out);
+
+	for (s = v->str, tmp = out; *s; s++, tmp++) {
+		unsigned char c = *s;
+
+		if (isalnum(c)) {
+			*tmp = c;
+			continue;
+		}
+
+		switch (c) {
+			case ' ':
+				*tmp = '+';
+				continue;
+			case '-':
+			case '_':
+			case '.':
+			case '~':
+				*tmp = c;
+				continue;
+		}
+
+		/* this char will be escaped */
+		tmp[0] = '%';
+		tmp[1] = hd[c >> 4];
+		tmp[2] = hd[c & 0xf];
+
+		tmp += 2;
+	}
+
+	*tmp = '\0';
+
+	v = malloc(sizeof(struct var_val));
+	assert(v);
+
+	v->type = VT_STR;
+	v->str  = out;
+
 	return v;
 }
 
@@ -43,6 +118,7 @@ static struct var_val *date_fxn(struct var_val *v)
 }
 
 static const struct pipestages stages[] = {
+	{ "urlescape", urlescape_fxn, },
 	{ "time", time_fxn, },
 	{ "date", date_fxn, },
 	{ NULL, },
