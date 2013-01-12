a0e32b3edbc0f75d4768eda1654e46408afd662c
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2013-01-12 21:15:26 +0000

    feed: figure out the last update time

--- feed.c
+++ feed.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <assert.h>
 
 #include "main.h"
 #include "config.h"
@@ -18,21 +19,37 @@ static int __render_page(struct req *req, char *tmpl)
 
 static void __load_posts(struct req *req, int page)
 {
+	struct var_val vv;
 	sqlite3_stmt *stmt;
+	time_t maxtime;
 	int ret;
 
+	maxtime = 0;
+
 	open_db();
-	SQL(stmt, "SELECT id FROM posts ORDER BY time DESC LIMIT ? OFFSET ?");
+	SQL(stmt, "SELECT id, strftime(\"%s\", time) FROM posts ORDER BY time DESC LIMIT ? OFFSET ?");
 	SQL_BIND_INT(stmt, 1, FEED_INDEX_STORIES);
 	SQL_BIND_INT(stmt, 2, page * FEED_INDEX_STORIES);
 	SQL_FOR_EACH(stmt) {
+		time_t posttime;
 		int postid;
 
-		postid = SQL_COL_INT(stmt, 0);
+		postid   = SQL_COL_INT(stmt, 0);
+		posttime = SQL_COL_INT(stmt, 1);
 
 		if (load_post(req, postid))
 			continue;
+
+		if (posttime > maxtime)
+			maxtime = posttime;
 	}
+
+	memset(&vv, 0, sizeof(vv));
+
+	vv.type = VT_INT;
+	vv.i    = maxtime;
+
+	assert(!var_append(&req->vars, "lastupdate", &vv));
 }
 
 static int __feed(struct req *req)
