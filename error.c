4528fcb10ee095a1e8b7613438ea5a4840093ed5
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2013-01-19 20:33:53 +0000

    use the OS-provided 'assfail' function

---
+++ error.c
@@ -0,0 +1,37 @@
+#include "error.h"
+
+#include <sys/debug.h>
+#include <syslog.h>
+#include <stdarg.h>
+
+void __my_log(const char *fmt, ...)
+{
+	va_list ap;
+	char msg[512];
+
+	va_start(ap, fmt);
+
+	vsnprintf(msg, sizeof(msg), fmt, ap);
+
+	syslog(LOG_LOCAL0 | LOG_CRIT, "%s", msg);
+	fprintf(stderr, "%s\n", msg);
+}
+
+int __my_assfail(const char *a, const char *f, int l)
+{
+	LOG("assertion failed: %s, file: %s, line: %d", a, f, l);
+
+	return assfail(a, f, l);
+}
+
+int __my_assfail3(const char *a, uintmax_t lv, const char *op, uintmax_t rv,
+		  const char *f, int l)
+{
+	char msg[512];
+
+	snprintf(msg, sizeof(msg), "%s (0x%lx %s 0x%lx)", a, lv, op, rv);
+
+	LOG("assertion failed: %s, file: %s, line: %d", msg, f, l);
+
+	return assfail(msg, f, l);
+}
--- error.h
+++ error.h
@@ -3,46 +3,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <syslog.h>
 
-#define LOG(...)	\
-	syslog(LOG_LOCAL0 | LOG_CRIT, __VA_ARGS__)
+extern void __my_log(const char *fmt, ...);
+extern int __my_assfail(const char *a, const char *f, int l);
+extern int __my_assfail3(const char *a, uintmax_t lv, const char *op,
+			 uintmax_t rv, const char *f, int l);
 
-static inline void __assfail(const char *assertion, const char *file,
-			     int line, const char *func)
-{
-	char msg[512];
-
-	snprintf(msg, sizeof(msg), "Assertion failed: %s, file %s, line %d, "
-		 "function %s", assertion, file, line, func);
-
-	LOG("%s", msg);
-	fprintf(stderr, "%s\n", msg);
-
-#if 0
-	extern void __set_panicstr(const char *);
-	__set_panicstr(msg);
-#endif
-
-	abort();
-}
+#define LOG(...)	__my_log(__VA_ARGS__)
 
 #define ASSERT3U(l, op, r)						\
 	do {								\
 		uint64_t lhs = (l);					\
 		uint64_t rhs = (r);					\
-		if (!(hs op rhs)) {					\
-			char msg[128];					\
-			snprintf(msg, sizeof(msg), "%s %s %s (%#lx %s %#lx)", \
-				 #l, #op, #r, lhs, #op, rhs);		\
-			__assfail(msg, __FILE__, __LINE__, __func__);	\
-		}							\
+		if (!(hs op rhs))					\
+			__my_assfail3(#l " " #op " " #r, lhs, #op, rhs,	\
+				      __FILE__, __LINE__);		\
 	} while(0)
 
 #define ASSERT(c)							\
 	do {								\
 		if (!(c))						\
-			__assfail(#c, __FILE__, __LINE__, __func__);	\
+			__my_assfail(#c, __FILE__, __LINE__);		\
 	} while(0)
 
 #endif
