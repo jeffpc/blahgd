fa81f8a99b6d60d43230d46afd84bf65b3b682c3
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2013-01-24 19:44:53 +0000

    listing: use the htmlescape mangler

--- listing.c
+++ listing.c
@@ -4,78 +4,30 @@
 
 #include "listing.h"
 #include "utils.h"
+#include "mangle.h"
 
 #define PAGE 4096
-
-#define COPYSTR(ptr, len, size, str)	do { \
-						int x = strlen(str); \
-						if ((len+x+1) > size) { \
-							size += PAGE; \
-							ptr = realloc(ptr, size); \
-						} \
-						strcpy(ptr+len, str); \
-						len += x; \
-					} while(0)
-
-#define COPYCHR(ptr, len, size, chr)	do { \
-						if ((len+2) > size) { \
-							size += PAGE; \
-							ptr = realloc(ptr, size); \
-						} \
-						ptr[len] = chr; \
-						ptr[len+1] = '\0'; \
-						len++; \
-					} while(0)
 
 char *listing(struct post *post, char *fname)
 {
 	char path[FILENAME_MAX];
-	char *out;
-	int len, size;
-	FILE *in;
+	char *in;
+	char *tmp;
 
 	snprintf(path, FILENAME_MAX, "data/posts/%d/%s", post->id, fname);
 
-	in = fopen(path, "r");
-	if (!in) {
-		snprintf(path, FILENAME_MAX, "Failed to open listing file '%s'.", fname);
-		return xstrdup(path);
-	}
+	in = read_file(path);
+	if (!in)
+		goto err;
 
-	len = 0;
-	size = PAGE;
-	out = malloc(PAGE);
-	if (!out)
-		goto err_mem;
+	tmp = mangle_htmlescape(in);
 
-	while(1) {
-		char c;
+	free(in);
 
-		c = fgetc(in);
-		if (feof(in))
-			break;
+	return tmp;
 
-		switch(c) {
-			case '&':
-				COPYSTR(out, len, size, "&amp;");
-				break;
-			case '<':
-				COPYSTR(out, len, size, "&lt;");
-				break;
-			case '>':
-				COPYSTR(out, len, size, "&gt;");
-				break;
-			default:
-				COPYCHR(out, len, size, c);
-				break;
-		}
-	}
-
-	fclose(in);
-	return out;
-
-err_mem:
-	snprintf(path, FILENAME_MAX, "Falied to allocate memory for buffer "
-		 "(len %d, size %d bytes).", len, size);
+err:
+	snprintf(path, FILENAME_MAX, "Failed to read in listing '%d/%s'",
+		 post->id, fname);
 	return xstrdup(path);
 }
