d326ea7b291ba0df7ef1c5dc8102c23a4bd06085
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2013-04-12 02:04:39 +0000

    admin page plan of attack

--- admin.c
+++ admin.c
@@ -4,5 +4,14 @@ int blahg_admin(struct req *req)
 {
 	req_head(req, "Content-Type", "text/plain");
 
+	/*
+	 * FIXME:
+	 * Loop through data/pending-comments dir entries.  For each entry,
+	 * output a table row that has all the information.  IOW,
+	 *  - pending-comments subdir name
+	 *  - dump the meta sql
+	 *  - dump the post body
+	 */
+
 	return 0;
 }
