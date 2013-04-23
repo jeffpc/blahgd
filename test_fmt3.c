de40bf07cdff55bd3576f241aa2dc1428c83bf6e
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2013-04-23 02:20:39 +0000

    CTest harness for fmt3 parser testing

---
+++ test_fmt3.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+
+#include "parse.h"
+#include "error.h"
+#include "utils.h"
+
+static int onefile(struct post *post, char *ibuf, size_t len)
+{
+	struct parser_output x;
+	int ret;
+
+	x.req   = NULL;
+	x.post  = post;
+	x.input = ibuf;
+	x.len   = strlen(ibuf);
+	x.pos   = 0;
+
+	post->table_nesting = 0;
+
+	fmt3_lex_init(&x.scanner);
+	fmt3_set_extra(&x, x.scanner);
+
+	ret = fmt3_parse(&x);
+
+	ASSERT(x.output);
+	free(x.output);
+
+	fmt3_lex_destroy(x.scanner);
+
+	return ret;
+}
+
+int main(int argc, char **argv)
+{
+	struct post post;
+	char *in;
+	int i;
+	int result;
+
+	result = 0;
+
+	for (i = 1; i < argc; i++) {
+		in = read_file(argv[i]);
+		ASSERT(in);
+
+		if (onefile(&post, in, strlen(in)))
+			result = 1;
+
+		free(in);
+	}
+
+	return result;
+}
