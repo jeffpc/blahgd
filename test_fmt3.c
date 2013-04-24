53fd85e84b7a79a1b21486b726c7857bc120b32c
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2013-04-24 03:50:38 +0000

    test_fmt3: don't try to print/free output on error

--- test_fmt3.c
+++ test_fmt3.c
@@ -21,10 +21,13 @@ static int onefile(struct post *post, char *ibuf, size_t len)
 	fmt3_set_extra(&x, x.scanner);
 
 	ret = fmt3_parse(&x);
-
-	ASSERT(x.output);
-	printf("%s", x.output);
-	free(x.output);
+	if (!ret) {
+		ASSERT(x.output);
+		printf("%s", x.output);
+		free(x.output);
+	} else {
+		fprintf(stderr, "failed to parse\n");
+	}
 
 	fmt3_lex_destroy(x.scanner);
 
