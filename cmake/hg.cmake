c5a66e0bf77ec06b5fa953864d16d662571b1d34
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2019-02-01 21:02:48 -0500

    version: use + to separate latest tag and the latest tag distance
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

--- cmake/hg.cmake
+++ cmake/hg.cmake
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2017 Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
+# Copyright (c) 2017-2019 Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -42,7 +42,7 @@ gethgrev(HG_COMMIT "{node}")
 
 # TODO: if there are uncommitted changes, append -dirty
 gethgrev(VERSION
-	"{ifeq(latesttagdistance,0,\"{latesttag}\",\"{latesttag}-{latesttagdistance}-{node|short}\")}")
+	"{ifeq(latesttagdistance,0,\"{latesttag}\",\"{latesttag}+{latesttagdistance}-{node|short}\")}")
 
 if(${HG_COMMIT} STREQUAL "unknown")
 	message(WARNING "Failed to get revision info from Mercurial.")
